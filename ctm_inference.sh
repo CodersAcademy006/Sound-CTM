3e995abca7dfc0f4f19d48406eccd640b33c5048
Srijan Upadhyay <104912634+CodersAcademy006@users.noreply.github.com>
2024-10-08 12:29:20 -0400

    Add files via upload

---
+++ ctm_inference.sh
@@ -0,0 +1,12 @@
+export CUDA_VISIBLE_DEVICES=0 
+
+python ctm_inference.py \
+    --text_encoder_name "google/flan-t5-large" \
+    --ctm_unet_model_config "configs/diffusion_model_config.json" \
+    --training_args '/path/to/checkpoints/soundctm_ckpt/030000/summary.jsonl' \
+    --model "/path/to/checkpoints/soundctm_ckpt/030000/pytorch_model.bin" \
+    --ema_model "/path/to/checkpoints/soundctm_ckpt/030000/ema_0.999_030000.pt" \
+    --test_file "/path/to/audiocaps/test.csv" \
+    --sampler 'determinisitc' --sampling_gamma 0. --omega 3.5 \
+    --num_steps 2 --nu 1. --num_samples 1 --batch_size 1 --test_references '/path/to/audiocaps/test' \
+    --output_dir "/path/to/output_dir/"
